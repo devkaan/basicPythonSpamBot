059ea791ec8ee4ee2a6fbdd559c9a25cb5d38c26
kaan <44449540+kaa01001110@users.noreply.github.com>
2020-11-29 12:38:36 +0300

    Update spammer.py

--- venv/spammer.py
+++ venv/spammer.py
@@ -6,16 +6,14 @@ import time
 Keyboard = Controller()
 time.sleep(sec)
 a = 1
-control = True
-while control:
+while true:
     b = str(a)
     if a == 6: # loop limit
         break
     for x in "hey, this is spam":
         Keyboard.press(x)
         Keyboard.release(x)
-    a+=1
+    a+=1 # or a++ -idk-
     time.sleep(0.5) # message time per second || 0.5 = 500ms  
     Keyboard.press(Key.enter)
     Keyboard.release(Key.enter)
-
